c0d3cfb1c5079c47f8fb4c5a5cb5436b10dabd74
George Giosmas <gegiosmas@gmail.com>
2026-09-12 16:01:38 +0300

    Fixes in the port/ functions regarding their core structure

--- port/stm32f4/stm32_i2c_bus.hpp
+++ port/stm32f4/stm32_i2c_bus.hpp
@@ -1,4 +1,42 @@
+#include <cstring>
+#include <cstdint>
+#include "/home/georgegio/stm32-i2c-cpp-driver/legacy/inc/stm32f4xx_hal_i2c.h"
+
 enum Class ReturnStatus: uint8_t{
     EXECUTION_SUCCEEDED,
     EXECUTION_FAILED
+};
+
+// I2C C++ Driver
+class Stm32i2cBus()
+{
+private:
+    mutable std::mutex mtx;
+public:
+    // Constructor
+    I2C_Driver_Cpp(){
+        I2C_HandleTypeDef *i2c = { 0 };
+    };
+
+    // Init Driver
+    ReturnStatus I2C_Driver_Cpp_Init(){}
+
+    // Configure Driver
+    ReturnStatus I2C_Driver_Cpp_Configure(I2C_HandleTypeDef& hi2c, I2C_TypeDef& Instance){}
+
+    // Transmit Data -> HAL_I2C_Master/Slaver_Transmit(...)
+
+
+    // Receive Data -> HAL_I2C_Master/Slaver_Receive(...)
+
+
+    // Error Handling -> ??? Will see if we can fir anything here
+
+
+    // Deconstructor + Deinit Driver
+    ~I2C_Driver_Cpp(){
+        extern "C"{
+            HAL_I2C_DeInit(hi2c);
+        }
+    }
 };
